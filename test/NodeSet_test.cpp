e822d0579b23941799a46935e88669b235ea58d3
David Andrs <andrsd@gmail.com>
2022-08-03 10:16:08 -0600

    Tests for NodeSet

---
+++ test/NodeSet_test.cpp
@@ -0,0 +1,28 @@
+#include "gmock/gmock.h"
+#include "exodusIIcpp.h"
+#include <stdexcept>
+
+using namespace exodusIIcpp;
+
+TEST(NodeSetTest, test)
+{
+    NodeSet ns;
+    EXPECT_EQ(ns.get_id(), -1);
+    EXPECT_EQ(ns.get_size(), 0);
+
+    ns.set_id(201);
+    EXPECT_EQ(ns.get_id(), 201);
+
+    ns.set_name("node_set");
+    EXPECT_STREQ(ns.get_name().c_str(), "node_set");
+
+    std::vector<int> nodes = { 1, 2, 3 };
+    ns.set_nodes(nodes);
+    EXPECT_EQ(ns.get_size(), 3);
+
+    EXPECT_THAT(ns.get_node_id(0), 1);
+    EXPECT_THAT(ns.get_node_id(1), 2);
+    EXPECT_THAT(ns.get_node_id(2), 3);
+
+    EXPECT_THROW(ns.get_node_id(3), std::out_of_range);
+}
