b1317a4129b65ea754f546aa268e6a649d2bbfa3
David Andrs <andrsd@gmail.com>
2022-08-05 10:06:46 -0600

    Starting doco

---
+++ docs/conf.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import subprocess
+
+project = 'exodusIIcpp'
+copyright = '2022, David Andrs'
+author = 'David Andrs'
+
+templates_path = ['_templates']
+
+exclude_patterns = []
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_show_sourcelink = False
---
+++ docs/index.rst
@@ -0,0 +1,4 @@
+exodusIIcpp
+===========
+
+`exodusIIcpp` is a simple library that is wrapping exodusII API into C++ classes.
